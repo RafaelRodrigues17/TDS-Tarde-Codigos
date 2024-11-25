ff4632172d968cf1fb419b08fdfb54dd42b006c0
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2024-11-25 10:53:53 -0300

    Create Sistema.java

---
+++ Códigos Java/Teatro Mágico 3.0(Versão Final)/Sistema.java
@@ -0,0 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Sistema {
+
+    public static void registrarEvento(String evento) {
+        String timestamp = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date());
+        String log = "EVENTO: " + evento + " - Data: " + timestamp;
+
+        if (TeatroMagico.logIndex < TeatroMagico.logs.length) {
+            TeatroMagico.logs[TeatroMagico.logIndex++] = log;
+        } else {
+            System.out.println("Limite de logs atingido.");
+        }
+    }
+
+    public static void inicializacaoSistema() {
+        registrarEvento("Inicialização do sistema");
+    }
+
+    public static void encerramentoSistema() {
+        registrarEvento("Encerramento do sistema");
+    }
+}
