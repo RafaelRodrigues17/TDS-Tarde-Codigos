083eb5f179a5dc9109a14afa01acb5612454f38d
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-03-27 17:14:30 -0300

    Add files via upload

---
+++ Códigos HTML,  JavaScript e CSS/Banco de dados/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template # Importando a biblioteca Flask
+
+app = Flask(__name__) # Criando um objeto do Flask chamado 
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
