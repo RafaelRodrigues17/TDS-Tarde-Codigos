d53027c3e4c9423e67ab7ebbe3e4913b9f6b7649
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-06-13 17:00:08 -0300

    Add files via upload

---
+++ As Cartas Python/carta.py
@@ -0,0 +1,21 @@
+class Carta:
+    def __init__(self, carta):
+        pass
+    
+    def usar_carta():
+        pass
+
+class CartaAumento (Carta):
+    pass
+
+class CartaRoubo (Carta):
+    pass
+
+class CartaAtordoamento (Carta):
+    pass
+
+class CartaDano (Carta):
+    pass
+
+class CartaCura (Carta):
+    pass
