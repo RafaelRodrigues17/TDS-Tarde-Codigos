20e59eddc306a90ed815f0223f2f25792a5dec9b
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-06-24 16:41:35 -0300

    Add files via upload

---
+++ As Cartas Python 3.1/partida.py
@@ -0,0 +1,23 @@
+class Partida:
+    def __init__(self, turno, jogador1, jogador2, jogador_atual):
+        self.turno = None
+        self.jogador1 = jogador1
+        self.jogador2 = jogador2
+        self.jogador_atual = jogador_atual
+        self.jogo_ativo = True
+    
+    def iniciar_partida(self): # Função para iniciar a partida
+        print(f"A partida foi iniciada! A vez é do jogador: {self.jogador_atual}")
+        
+        # while self.jogo_ativo:
+        #     if self.jogador_atual == 1:
+                 
+    def trocar_turno():
+        pass
+    
+    def trocar_Jogador(): # Troca o jagador
+        pass
+    
+    def encerrar_patida(): # Finaliza a partida (finaliza quando um dos personagens morre)
+        pass
+    
